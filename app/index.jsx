a87b5ecee26d6ce40a4b6271b5a90a19b876c92c
Elias Kristmansson <elias.kristmansson97@gmail.com>
2025-09-16 09:52:14 +0200

    fixed pivot from typescript to jsx 2.0

--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,17 @@
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 32, fontWeight: "bold" }}>
-        Welcome to Songfrontation!
-      </Text>
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text style={{ fontSize: 32, fontWeight: "bold" }}>
+                Welcome to Songfrontation!
+            </Text>
+        </View>
+    );
 }
